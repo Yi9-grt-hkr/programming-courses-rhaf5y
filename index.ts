dfe79f95e4a7087afdc7a70bf0b09a8adb5ec93d
Yi9-grt-hkr <hisabamriki34@gmail.com>
2021-04-03 10:35:54 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@
 //###########################   Programming Courses  ###################
 //######################################################################
 console.log("Bonjour tout le monde");
+console.log("aire force of algeria");
+console.log("Algeria wins the world cup");
